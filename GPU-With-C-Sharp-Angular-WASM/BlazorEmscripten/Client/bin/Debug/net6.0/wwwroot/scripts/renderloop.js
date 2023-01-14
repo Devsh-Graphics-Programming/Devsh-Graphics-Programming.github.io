b5aca070ed7571154397a9808fe195524ec9050e
AnastaZIuk <areklachowicz@gmail.com>
2023-01-14 16:39:27 +0100

    update manually with new Emscripten projects and all configs

---
+++ GPU-With-C-Sharp-Angular-WASM/BlazorEmscripten/Client/bin/Debug/net6.0/wwwroot/scripts/renderloop.js
@@ -0,0 +1,17 @@
+﻿//By default,
+//the canvas will update only when user provides input. (i.e. when clicking a mouse button or moving the mouse)
+//As a result
+//The scene will not draw new frames when user is idle
+
+//to prevent this, JS will periodically invoke C# function to keep rendering
+renderLoop = () => {
+    window.csharpInstance.invokeMethod('RenderInBlazor');
+    window.requestAnimationFrame(renderLoop);
+
+}
+
+//initRenderLoop - function called from Pathtracing.razor after Renderer object was created
+initRenderLoop = (csInstance) => {
+    window.csharpInstance = csInstance;
+    window.requestAnimationFrame(renderLoop);
+}
